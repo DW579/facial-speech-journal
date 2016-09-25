e30c6f8baa8c347615df5a32fb37c7b3d934bdb1
Dustin Wurtz <dtwurtz@gmail.com>
2016-09-24 22:11:14 -0700

    Fixed deprecated express middleware solution

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.exports = function(app) {
 
   app.get('*', function(req, res) {
-        res.sendfile('./public/index.html');
+        res.sendFile(path.join(__dirname, '../public', 'index.html'));
     });
-    
+
 }
