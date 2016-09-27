a3deb76b3ce243de33ffb2802c34bba074bd4e17
Dustin Wurtz <dtwurtz@gmail.com>
2016-09-26 20:28:18 -0700

    Implmented recorded video

--- public/js/controllers/analyzeCtrl.js
+++ public/js/controllers/analyzeCtrl.js
@@ -6,28 +6,144 @@ function AnalyzeController() {
 
   'use strict';
 
-  navigator.getUserMedia = navigator.getUserMedia ||
-      navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  var mediaSource = new MediaSource();
+  mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
+  var mediaRecorder;
+  var recordedBlobs;
+  var sourceBuffer;
+
+  var gumVideo = document.querySelector('video#gum');
+  var recordedVideo = document.querySelector('video#recorded');
+
+  var recordButton = document.querySelector('button#record');
+  var playButton = document.querySelector('button#play');
+  var downloadButton = document.querySelector('button#download');
+  recordButton.onclick = toggleRecording;
+  playButton.onclick = play;
+  downloadButton.onclick = download;
+
+  var isSecureOrigin = location.protocol === 'https:' ||
+  location.hostname === 'localhost';
+  if (!isSecureOrigin) {
+    alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
+      '\n\nChanging protocol to HTTPS');
+    location.protocol = 'HTTPS';
+  }
+
 
   var constraints = {
-    audio: false,
+    audio: true,
     video: true
   };
-  var video = document.querySelector('video');
 
-  function successCallback(stream) {
-    window.stream = stream; // stream available to console
+  function handleSuccess(stream) {
+    console.log('getUserMedia() got stream: ', stream);
+    window.stream = stream;
     if (window.URL) {
-      video.src = window.URL.createObjectURL(stream);
+      gumVideo.src = window.URL.createObjectURL(stream);
     } else {
-      video.src = stream;
+      gumVideo.src = stream;
     }
   }
 
-  function errorCallback(error) {
+  function handleError(error) {
     console.log('navigator.getUserMedia error: ', error);
   }
 
-  navigator.getUserMedia(constraints, successCallback, errorCallback);
+  navigator.mediaDevices.getUserMedia(constraints).
+      then(handleSuccess).catch(handleError);
+
+  function handleSourceOpen(event) {
+    console.log('MediaSource opened');
+    sourceBuffer = mediaSource.addSourceBuffer('video/wmv; codecs="vp8"');
+    console.log('Source buffer: ', sourceBuffer);
+  }
+
+  recordedVideo.addEventListener('error', function(ev) {
+    console.error('MediaRecording.recordedMedia.error()');
+    alert('Your browser can not play\n\n' + recordedVideo.src
+      + '\n\n media clip. event: ' + JSON.stringify(ev));
+  }, true);
+
+  function handleDataAvailable(event) {
+    if (event.data && event.data.size > 0) {
+      recordedBlobs.push(event.data);
+    }
+  }
+
+  function handleStop(event) {
+    console.log('Recorder stopped: ', event);
+  }
+
+  function toggleRecording() {
+    if (recordButton.textContent === 'Start Recording') {
+      startRecording();
+    } else {
+      stopRecording();
+      recordButton.textContent = 'Start Recording';
+      playButton.disabled = false;
+      downloadButton.disabled = false;
+    }
+  }
+
+  function startRecording() {
+    recordedBlobs = [];
+    var options = {mimeType: 'video/wmv;codecs=vp9'};
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+      console.log(options.mimeType + ' is not Supported');
+      options = {mimeType: 'video/wmv;codecs=vp8'};
+      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.log(options.mimeType + ' is not Supported');
+        options = {mimeType: 'video/wmv'};
+        if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+          console.log(options.mimeType + ' is not Supported');
+          options = {mimeType: ''};
+        }
+      }
+    }
+    try {
+      mediaRecorder = new MediaRecorder(window.stream, options);
+    } catch (e) {
+      console.error('Exception while creating MediaRecorder: ' + e);
+      alert('Exception while creating MediaRecorder: '
+        + e + '. mimeType: ' + options.mimeType);
+      return;
+    }
+    console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+    recordButton.textContent = 'Stop Recording';
+    playButton.disabled = true;
+    downloadButton.disabled = true;
+    mediaRecorder.onstop = handleStop;
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(10); // collect 10ms of data
+    console.log('MediaRecorder started', mediaRecorder);
+  }
+
+  function stopRecording() {
+    mediaRecorder.stop();
+    console.log('Recorded Blobs: ', recordedBlobs);
+    recordedVideo.controls = true;
+  }
+
+  function play() {
+    var superBuffer = new Blob(recordedBlobs, {type: 'video/wmv'});
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+  }
+
+  function download() {
+    var blob = new Blob(recordedBlobs, {type: 'video/wmv'});
+    var url = window.URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'test.wmv';
+    console.log("downloaded file: " + a.download);
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function() {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 100);
+  }
 
 }
